935d6f83d760796523acdf62eac77ba3367d9a1d
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-12-30 15:48:28 -0500

    Initial commit

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    [SerializeField] float distanceToPlayer;    
+    [SerializeField] float moveSpeed;
+    bool hasBoarded;
+    [SerializeField] GameObject destination;
+    [SerializeField] float distanceToDestination;
+    bool hasArrived;
+    Collider2D collider;
+    [SerializeField] Material black;
+
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < distanceToPlayer && PlayerController.instance.HasLanded() && hasBoarded == false)
+        {
+            destination.GetComponent<MeshRenderer>().material = black;
+            transform.position = Vector3.MoveTowards(transform.position, PlayerController.instance.transform.position, moveSpeed * Time.deltaTime);
+        }
+
+        if (hasBoarded)
+            transform.position = PlayerController.instance.transform.position;
+
+        if (Vector3.Distance(transform.position, destination.transform.position) < distanceToDestination && PlayerController.instance.HasLanded())
+        {
+            hasArrived = true;
+            hasBoarded = false;
+        }
+
+        if (hasArrived)
+        {
+            collider.enabled = false;
+            transform.position = Vector3.MoveTowards(transform.position, destination.transform.position, moveSpeed * Time.deltaTime);
+            //todo colliders are colliding together, which means the NPC does not move until player has flown away
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && hasArrived == false)
+            hasBoarded = true;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    Rigidbody2D rb;
+    [SerializeField] float thrustForce;
+    [SerializeField] float moveSpeed;
+    bool hasLanded;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.AddForce(transform.up * thrustForce * Input.GetAxis("Vertical"));
+        if (!hasLanded)
+            rb.AddForce(transform.right * moveSpeed * Input.GetAxis("Horizontal")); 
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+            hasLanded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+            hasLanded = false;
+    }
+
+    public bool HasLanded()
+    {
+        return hasLanded;
+    }
+}
