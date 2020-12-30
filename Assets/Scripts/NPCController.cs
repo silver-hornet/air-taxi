eae59b77b6ec1d033194880f76a1ebececbd8591
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-12-30 16:49:43 -0500

    Refactored PlayerController and NPCController

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -2,43 +2,53 @@
 
 public class NPCController : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] GameObject destination;
+    [SerializeField] Material color; // todo replace with a sprite and make the change to the player and door through code instead
+
+    [Header("Config")]
     [SerializeField] float distanceToPlayer;    
     [SerializeField] float moveSpeed;
-    bool hasBoarded;
-    [SerializeField] GameObject destination;
     [SerializeField] float distanceToDestination;
-    bool hasArrived;
-    Collider2D collider;
-    [SerializeField] Material black;
 
-    void Start()
+    [Header("Bools")]
+    bool hasBoarded;
+    bool hasArrived;
+    
+    void Update()
     {
-        collider = GetComponent<Collider2D>();
+        Boarding();
+        Arriving();
     }
 
-    void Update()
+    private void Boarding()
     {
         if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < distanceToPlayer && PlayerController.instance.HasLanded() && hasBoarded == false)
         {
-            destination.GetComponent<MeshRenderer>().material = black;
+            destination.GetComponent<MeshRenderer>().material = color;
             transform.position = Vector3.MoveTowards(transform.position, PlayerController.instance.transform.position, moveSpeed * Time.deltaTime);
         }
 
         if (hasBoarded)
             transform.position = PlayerController.instance.transform.position;
+    }
 
+    private void Arriving()
+    {
         if (Vector3.Distance(transform.position, destination.transform.position) < distanceToDestination && PlayerController.instance.HasLanded())
-        {
+        { // todo bug: when crashing sideways into a nearby vertical platform
             hasArrived = true;
             hasBoarded = false;
         }
 
         if (hasArrived)
         {
-            collider.enabled = false;
             transform.position = Vector3.MoveTowards(transform.position, destination.transform.position, moveSpeed * Time.deltaTime);
-            //todo colliders are colliding together, which means the NPC does not move until player has flown away
+            //todo bug: colliders are colliding together, which means the NPC does not move until player has flown away
         }
+
+        if (transform.position == destination.transform.position && hasArrived)
+            Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D other)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,18 @@
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("References")]
     public static PlayerController instance;
     Rigidbody2D rb;
+
+    [Header("Config")]
     [SerializeField] float thrustForce;
     [SerializeField] float moveSpeed;
+    [SerializeField] float dangerousSpeed;
+    float currentSpeedX;
+    float currentSpeedY;
+
+    [Header("Bools")]
     bool hasLanded;
 
     void Awake()
@@ -22,13 +30,21 @@ public class PlayerController : MonoBehaviour
     {
         rb.AddForce(transform.up * thrustForce * Input.GetAxis("Vertical"));
         if (!hasLanded)
-            rb.AddForce(transform.right * moveSpeed * Input.GetAxis("Horizontal")); 
+            rb.AddForce(transform.right * moveSpeed * Input.GetAxis("Horizontal"));
+        currentSpeedX = rb.velocity.x;
+        currentSpeedY = rb.velocity.y;
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Platform"))
             hasLanded = true;
+
+        if (currentSpeedX < -dangerousSpeed || currentSpeedX > dangerousSpeed)
+            Debug.Log("Crash!");
+
+        if (currentSpeedY < -dangerousSpeed || currentSpeedY > dangerousSpeed)
+            Debug.Log("Crash!");
     }
 
     void OnCollisionExit2D(Collision2D other)
